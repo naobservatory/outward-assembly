f36dab9b7767077a79cc28cd2598810a0323e814
Evan Fields <evan@securebio.org>
2025-04-15 14:42:12 +0000

    remove broken reference to deprecated function

--- outward_assembly/__init__.py
+++ outward_assembly/__init__.py
@@ -2,13 +2,12 @@
 Outward assembly package for assembling genomic context around a seed sequence.
 """
 
-from .pipeline import outward_assembly, outward_assembly_eval_challenge
+from .pipeline import outward_assembly
 from .kmer_freq_filter import frequency_filter_reads
 from .io_helpers import s3_files_with_prefix, process_s3_paths, dir_to_s3_paths
 
 __all__ = [
     "outward_assembly",
-    "outward_assembly_eval_challenge",
     "frequency_filter_reads",
     "s3_files_with_prefix",
     "process_s3_paths",
